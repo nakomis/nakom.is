c170082ed24b74b5bb5070c24272e3adc6267b33
Martin Harris <1488244+nakomis@users.noreply.github.com>
2024-05-04 20:09:25 +0100

    Adds urlshortener lambda

---
+++ lambda/urlshortener.py
@@ -0,0 +1,76 @@
+import boto3
+
+def lambda_handler(event, context):
+    if 'pathParameters' not in event.keys():
+        return google()
+
+    pp = event["pathParameters"]
+    
+    if (('thepath' not in pp.keys()) or ("" == pp["thepath"])):
+        return google()
+        
+    path = pp["thepath"]
+    
+    if "robots.txt" == path:
+        return robots()
+
+    if path.startswith("cat") and len(path) > 3:
+        return cat(path)
+
+    client = boto3.client('dynamodb')
+    dbdata = client.get_item(TableName='redirects', Key={'shortPath': {'S': path}})
+    
+    if 'Item' in dbdata.keys():
+        url = dbdata["Item"]["url"]["S"]
+        hitcount = int(dbdata["Item"]["hitCount"]["N"])
+        hitcount = hitcount + 1
+        client.put_item(TableName='redirects', Item={'shortPath': {'S': path}, 'hitCount': {'N': str(hitcount)}, 'url': {'S': url}})
+        if not url.startswith('http'):
+            url = "https://" + url
+        print("Redirecting: " + path)
+        return  {
+                    'statusCode': 301,
+                    'headers': {
+                        "Location": url
+                    }
+                }
+                
+    return bailout(path)
+
+def bailout(path):
+    print("Bailing out: " + path)
+    return {
+                'statusCode': 301,
+                'headers': {
+                    "Location": 'https://www.google.co.uk/search?q=' + path
+                }
+            }
+
+def google():
+    print("Defaulting to Google:")
+    return {
+                'statusCode': 301,
+                'headers': {
+                    "Location": 'https://www.google.co.uk'
+                }
+            }
+
+def cat(path):
+    catPath = path[3:]
+    print("Redirecting to cat: " + catPath)
+    
+    return {
+                'statusCode': 301,
+                'headers': {
+                    "Location": 'https://http.cat/status/' + catPath
+                }
+            }
+
+def robots():
+    print("Robots")
+    return {
+        'statusCode': 200,
+        'body': """User-agent: *
+Disallow: /
+"""
+    }
--- lib/nakom.is-stack.ts
+++ lib/nakom.is-stack.ts
@@ -1,6 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { AttributeType, TableV2 } from 'aws-cdk-lib/aws-dynamodb';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class NakomIsStack extends cdk.Stack {
@@ -13,9 +15,19 @@ export class NakomIsStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'NakomIsQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
-    const table = new TableV2(this, 'redirects', {
+    const redirectTable = new TableV2(this, 'redirects', {
       tableName: 'redirects',
       partitionKey: { name: 'shortPath', type: AttributeType.STRING },
     });
+
+    const redirectsFunction = new lambda.Function(this, 'RedirectsFunction', {
+      functionName: 'urlShortener',
+      runtime: lambda.Runtime.PYTHON_3_9,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'urlshortener.lambda_handler'
+    });
+
+    redirectTable.grantReadWriteData(redirectsFunction);
+
   }
 }
