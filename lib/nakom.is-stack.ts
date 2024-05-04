06e4826dbe1a2a66f3eb5a82c04073eb1e43383a
Martin Harris <1488244+nakomis@users.noreply.github.com>
2024-05-04 22:05:20 +0100

    Adds API Gateway, and robots.txt resource

--- lib/nakom.is-stack.ts
+++ lib/nakom.is-stack.ts
@@ -2,19 +2,13 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { AttributeType, TableV2 } from 'aws-cdk-lib/aws-dynamodb';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as api from 'aws-cdk-lib/aws-apigateway';
 
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class NakomIsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'NakomIsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
     const redirectTable = new TableV2(this, 'redirects', {
       tableName: 'redirects',
       partitionKey: { name: 'shortPath', type: AttributeType.STRING },
@@ -28,6 +22,39 @@ export class NakomIsStack extends cdk.Stack {
     });
 
     redirectTable.grantReadWriteData(redirectsFunction);
+
+    const gateway = new api.RestApi(this, 'RestApi', {
+      restApiName: 'nakom.is'
+    });
+
+    const robotsIntegration = new api.MockIntegration({
+      integrationResponses: [
+        {
+          statusCode: "200",
+          contentHandling: api.ContentHandling.CONVERT_TO_TEXT,
+          responseTemplates: {
+            'text/plain': 'User-agent: *\nDisallow: /\n'
+          },
+          responseParameters: {
+            "method.response.header.Content-Type": "'text/plain'"
+          }
+        }
+      ],
+      requestTemplates: {
+        'application/json': '{"statusCode": 200}'
+      }
+    });
+
+    const getRobots = gateway.root.addResource('robots.txt').addMethod('GET', robotsIntegration, {
+      methodResponses: [
+        {
+          responseParameters: {
+            "method.response.header.Content-Type": true
+          },
+          statusCode: '200'
+        }
+      ]
+    });
 
   }
 }
