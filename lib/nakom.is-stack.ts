718389a903d901084755404ca85a24296f786c98
Martin Harris <1488244+nakomis@users.noreply.github.com>
2024-05-05 15:28:36 +0100

    Implements API Gateway Exceptions

--- lib/nakom.is-stack.ts
+++ lib/nakom.is-stack.ts
@@ -11,12 +11,17 @@ export interface NakomIsStackProps extends cdk.StackProps {
 export class NakomIsStack extends cdk.Stack {
     readonly anyIntegration: api.MockIntegration;
     readonly gateway: api.RestApi;
+    // FIXME: Create the role as part of this stack
+    readonly s3Role = Role.fromRoleArn(this, "S3Role", "arn:aws:iam::637423226886:role/MHnakom.isReadS3");
+    // FIXME: Create the bucket as part of a stack
+    readonly bucketName = 'mhtestfornakom.is';
 
     constructor(scope: Construct, id: string, props?: NakomIsStackProps) {
         super(scope, id, props);
 
         this.gateway = new api.RestApi(this, 'RestApi', {
-            restApiName: 'nakom.is'
+            restApiName: 'nakom.is',
+            binaryMediaTypes: ["*/*"],
         });
 
         this.anyIntegration = new api.MockIntegration({
@@ -31,7 +36,6 @@ export class NakomIsStack extends cdk.Stack {
         });
 
         this.addRoot();
-        this.addRobots();
         this.addStatic();
         this.addLambda(props!.urlShortener);
         this.addExceptions();
@@ -70,56 +74,17 @@ export class NakomIsStack extends cdk.Stack {
         this.addAny405(this.gateway.root);
     }
 
-    // FIXME: Add to `addExceptions`
-    addRobots() {
-        const robotsIntegration = new api.MockIntegration({
-            integrationResponses: [
-                {
-                    statusCode: "200",
-                    contentHandling: api.ContentHandling.CONVERT_TO_TEXT,
-                    responseTemplates: {
-                        'text/plain': 'User-agent: *\nDisallow: /\n'
-                    },
-                    responseParameters: {
-                        "method.response.header.Content-Type": "'text/plain'"
-                    }
-                }
-            ],
-            requestTemplates: {
-                'application/json': '{"statusCode": 200}'
-            }
-        });
-
-        const robotsResource = this.gateway.root.addResource('robots.txt');
-
-        const getRobots = robotsResource.addMethod('GET', robotsIntegration, {
-            methodResponses: [
-                {
-                    responseParameters: {
-                        "method.response.header.Content-Type": true
-                    },
-                    statusCode: '200'
-                }
-            ]
-        });
-
-        this.addAny405(robotsResource);
-    }
-
     addStatic() {
         const staticResource = this.gateway.root.addResource('static');
         const staticFileResource = staticResource.addResource('{file+}');
 
-        // FIXME: Create the role as part of this stack
-        const myrole = Role.fromRoleArn(this, "S3Role", "arn:aws:iam::637423226886:role/MHnakom.isReadS3");
-
         const staticFileIntegration = new api.AwsIntegration({
-            service: "s3",
-            path: "mhtestfornakom.is/{abc}",
+            service: 's3',
+            path: `${this.bucketName}/{s3file}`,
             options: {
                 passthroughBehavior: api.PassthroughBehavior.WHEN_NO_MATCH,
                 requestParameters: {
-                    "integration.request.path.abc": 'method.request.path.file'
+                    "integration.request.path.s3file": 'method.request.path.file'
                 },
                 integrationResponses: [
                     {
@@ -140,13 +105,13 @@ export class NakomIsStack extends cdk.Stack {
                         }
                     }
                 ],
-                credentialsRole: myrole
+                credentialsRole: this.s3Role
             },
             region: "eu-west-2",
             integrationHttpMethod: "GET",
         });
 
-        const staticFileOptions: api.MethodOptions = {
+        const staticMethodOptions: api.MethodOptions = {
             requestParameters: {
                 'method.request.path.file': true
             },
@@ -158,7 +123,10 @@ export class NakomIsStack extends cdk.Stack {
                         'method.response.header.Content-Type': false
                     },
                     responseModels: {
-                        "application/json": {
+                        "application/octet-stream": {
+                            modelId: "Empty"
+                        },
+                        "application/pdf": {
                             modelId: "Empty"
                         }
                     }
@@ -177,7 +145,7 @@ export class NakomIsStack extends cdk.Stack {
             ]
         }
 
-        staticFileResource.addMethod('GET', staticFileIntegration, staticFileOptions);
+        staticFileResource.addMethod('GET', staticFileIntegration, staticMethodOptions);
 
         this.addAny405(staticResource);
         this.addAny405(staticFileResource);
@@ -219,7 +187,7 @@ export class NakomIsStack extends cdk.Stack {
     }
 
     addExceptions() {
-        [
+        const exceptions = [
             {
                 "path": "cv",
                 "file": "cv.pdf"
@@ -227,9 +195,83 @@ export class NakomIsStack extends cdk.Stack {
             {
                 "path": "wordle",
                 "file": "wordle.html"
+            },
+            {
+                "path": "robots.txt",
+                "file": "robots.txt"
+            },
+            {
+                "path": "favicon.ico",
+                "file": "favicon.ico"
             }
-        ].forEach((exception) => {
+        ];
+
+        const exceptionMethodOptions: api.MethodOptions = {
+            methodResponses: [
+                {
+                    statusCode: "200",
+                    responseParameters: {
+                        'method.response.header.Content-Length': false,
+                        'method.response.header.Content-Type': false
+                    },
+                    responseModels: {
+                        "application/json": {
+                            modelId: "Empty"
+                        }
+                    }
+                },
+                {
+                    statusCode: "301",
+                    responseParameters: {
+                        'method.response.header.Location': true
+                    },
+                    responseModels: {
+                        "application/octet-stream": {
+                            modelId: "Empty"
+                        },
+                        "application/pdf": {
+                            modelId: "Empty"
+                        }
+                    }
+                }
+            ]
+        };
+
+        exceptions.forEach((exception) => {
             const exceptionalResource = this.gateway.root.addResource(exception.path);
+            const exceptionIntegration = new api.AwsIntegration({
+                service: 's3',
+                path: `${this.bucketName}/{s3file}`,
+                options: {
+                    passthroughBehavior: api.PassthroughBehavior.WHEN_NO_MATCH,
+                    requestParameters: {
+                        "integration.request.path.s3file": `'${exception.file}'`
+                    },
+                    integrationResponses: [
+                        {
+                            statusCode: "200",
+                            responseParameters: {
+                                'method.response.header.Content-Length': 'integration.response.header.Content-Length',
+                                'method.response.header.Content-Type': 'integration.response.header.Content-Type'
+                            }
+                        },
+                        {
+                            statusCode: "301",
+                            selectionPattern: "403",
+                            responseParameters: {
+                                'method.response.header.Location': "'https://www.google.co.uk'"
+                            },
+                            responseTemplates: {
+                                'text/plain': 'Not Found'
+                            }
+                        }
+                    ],
+                    credentialsRole: this.s3Role
+                },
+                region: "eu-west-2",
+                integrationHttpMethod: "GET",
+            });
+            exceptionalResource.addMethod("GET", exceptionIntegration, exceptionMethodOptions);
             this.addAny405(exceptionalResource);
         })
     }
