5e9289181357b142551ec55f398160cfff2a82e9
Martin Harris <1488244+nakomis@users.noreply.github.com>
2026-02-25 20:27:16 +0000

    Adds more info to social app footer

--- social-app/src/components/Footer.tsx
+++ social-app/src/components/Footer.tsx
@@ -3,9 +3,9 @@ import versionData from '../../version.json';
 
 function Footer() {
   return (
-    <div className="app-footer">
-      v{versionData.version} · <a href="https://nakom.is/nakom.is" target="_blank" rel="noreferrer">Open source</a>
-    </div>
+    <><div className="app-footer">
+          v{versionData.version} · Designed and built by Nakomis · Open-source <a href="https://creativecommons.org/public-domain/cc0/" target="_blank" rel="noreferrer">CC0</a> code available on <a href="https://nakom.is/nakom.is" target="_blank" rel="noreferrer">GitHub</a>.
+    </div></>
   );
 }
 
