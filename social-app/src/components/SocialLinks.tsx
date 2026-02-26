e0debf497297b94e8dee1fa9ed09290a89ff81a5
Martin Harris <1488244+nakomis@users.noreply.github.com>
2026-02-26 22:49:35 +0000

    feat: add blog link to social app and remove Twitter

--- social-app/src/components/SocialLinks.tsx
+++ social-app/src/components/SocialLinks.tsx
@@ -2,9 +2,9 @@ const links = [
   { id: 'link-cv', href: 'https://nakom.is/cv', icon: 'fas fa-file-alt', colorClass: 'cv', label: 'CV / Resume', connected: true },
   { id: 'link-linkedin', href: 'https://www.linkedin.com/in/nakomis', icon: 'fab fa-linkedin', colorClass: 'linkedin', label: 'LinkedIn', connected: true },
   { id: 'link-github', href: 'https://github.com/nakomis/', icon: 'fab fa-github', colorClass: 'github', label: 'GitHub', connected: true },
+  { id: 'link-blog', href: 'https://blog.nakom.is', icon: 'fas fa-rss', colorClass: 'blog', label: 'Blog', connected: true },
   { id: 'link-bluesky', href: 'https://bsky.app/profile/nakomis.com', icon: 'fas fa-cloud', colorClass: 'bluesky', label: 'Bluesky', connected: false },
   { id: 'link-facebook', href: 'https://www.facebook.com/Nakomis/', icon: 'fab fa-facebook', colorClass: 'facebook', label: 'Facebook', connected: false },
-  { id: 'link-twitter', href: 'https://x.com/nakomis', icon: 'fab fa-twitter', colorClass: 'twitter', label: '(fka) Twitter', connected: false },
 ];
 
 function SocialLinks() {
